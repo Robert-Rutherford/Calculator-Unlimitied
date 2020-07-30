fc914104cd86ec6bf7623818143d8c09de48c82e
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-07-30 12:21:18 -0500

    setup

---
+++ src/main/java/com/robertrutherford/calculatorunlimited/CalculatorunlimitedApplication.java
@@ -0,0 +1,13 @@
+package com.robertrutherford.calculatorunlimited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorunlimitedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorunlimitedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robertrutherford/calculatorunlimited/CalculatorunlimitedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertrutherford.calculatorunlimited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorunlimitedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
