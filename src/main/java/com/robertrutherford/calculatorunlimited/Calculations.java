e8e35fa9372d3af566c94e94aac209a8c2b66560
Robert Rutherford <robertl.rutherford2@gmail.com>
2020-07-31 12:44:29 -0500

    Java functions: bracket check function, and formula split function start

--- src/main/java/com/robertrutherford/calculatorunlimited/Calculations.java
+++ src/main/java/com/robertrutherford/calculatorunlimited/Calculations.java
@@ -12,6 +12,8 @@ public class Calculations {
             return validFunction(formula);
         }
 
+        char[] divFormula;
+
 
 
         return "";
@@ -19,10 +21,85 @@ public class Calculations {
 
     private String validFunction(String formula){
 
+        if (!bracketLogic(formula)){
+            return "Error: bracket conflict";
+        }
+
         return "true";
     }
 
+    private boolean bracketLogic(String xp) {
+        char[] startBrackets = {'(', '[', '{', '<'};
+        char[] endBrackets = {')', ']', '}', '>'};
+        int priorityIndex = 0;
+        char[] currentStack = new char[10];
+        for (int i = 0; i < xp.length(); i++) {
+//            open bracket case
+            boolean contains = false;
+            for (char c : startBrackets) {
+                if (c == xp.charAt(i)) {
+                    contains = true;
+                    break;
+                }
+            }
+            if (contains) {
+                currentStack[priorityIndex] = xp.charAt(i);
+                priorityIndex++;
+            }
 
+//            close bracket case
+            for (char c : endBrackets) {
+                if (c == xp.charAt(i)) {
+                    contains = true;
+                    break;
+                }
+            }
+            if (contains) {
+                if (priorityIndex == 0) {
+                    return false;
+                }
+                if (xp.charAt(i) == ')') {
+                    if (currentStack[priorityIndex - 1] != '(') {
+                        return false;
+                    } else {
+                        priorityIndex--;
+                    }
+                }
+                if (xp.charAt(i) == ']') {
+                    if (currentStack[priorityIndex - 1] != '[') {
+                        return false;
+                    } else {
+                        priorityIndex--;
+                    }
+                }
+                if (xp.charAt(i) == '}') {
+                    if (currentStack[priorityIndex - 1] != '{') {
+                        return false;
+                    } else {
+                        priorityIndex--;
+                    }
+                }
+                if (xp.charAt(i) == '>') {
+                    if (currentStack[priorityIndex - 1] != '<') {
+                        return false;
+                    } else {
+                        priorityIndex--;
+                    }
+                }
+            }
+
+        }
+        return priorityIndex == 0;
+    }
+
+    private char[] seperateFormula(String formula){
+        char[] splitFormula;
+
+
+
+
+        return splitFormula;
+    }
 
 
 }
